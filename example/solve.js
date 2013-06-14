d7b38a402ab46f6e53bf4d5bef2487d93095689f
James Halliday <mail@substack.net>
2013-06-14 05:34:39 -0700

    simple general-purpose solver

---
+++ example/solve.js
@@ -0,0 +1,12 @@
+var solver = require('../');
+var sequence = process.argv.slice(2).map(Number);
+
+var f = solver(sequence);
+console.log(pretty(f));
+
+function pretty (f) {
+    return (f+'').replace(/\n/g,'')
+        .replace(/^function anonymous\(n\) {return /, '')
+        .replace(/}$/, '')
+    ;
+}
