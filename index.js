7dee31e643d7f9bbfc9d08dcdbe3a9d3db5e694d
James Halliday <mail@substack.net>
2013-06-14 05:04:47 -0700

    integer divide

--- index.js
+++ index.js
@@ -28,11 +28,18 @@ function solve (xs) {
 
 function evaluate (terms) {
     var src = 'return '
-        + Array(terms.length).join('(')
+        + terms.map(function (t) {
+            if (t[0] === '/') return 'Math.floor(';
+            else return '(';
+        }).join('')
         + 'n'
         + terms.map(function (t) {
+            if (t[0] === '/') {
+                return t[0] + '(' + t[1] + ')';
+            }
             return t[0] + '(' + t[1] + ')';
         }).join(')')
+        + ')'
     ;
     return Function('n', src);
 }
