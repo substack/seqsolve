6012e55a4db484c3b47ebce2909d8b114913b359
James Halliday <mail@substack.net>
2013-06-14 04:32:01 -0700

    callback version of combinations()

--- example/combinations.js
+++ example/combinations.js
@@ -1,12 +1,13 @@
 var cf = require('continued-fraction');
 var solver = require('../');
 var ops = [ '+', '-', '*', '/' ];
-var constants = [ 1, 2, 'n' ];
+var nums = [ 1, 2, 'n' ];
 
 //var sequence = [ 0, 1, 6, 3, 1, 7, 2, 1, 3, 1, 1, 1, 2, 18, 1, 1, 1, 2 ];
 //var sequence = [ 0, 1, 2, 3, 4 ]; // SOLVED
 //var sequence = [ 2, 4, 8, 16, 32, 64 ]; // SOLVED
-var sequence = [ 1, 3, 5, 7, 9 ];
+//var sequence = [ 1, 3, 5, 7, 9 ];
+var sequence = process.argv.slice(2).map(Number);
 var best = 0;
 
 function check (f) {
@@ -26,31 +27,26 @@ function check (f) {
 
 var partials = [];
 ops.forEach(function (op) {
-    constants.forEach(function (c) {
-        partials.push([ op, c ]);
+    nums.forEach(function (n) {
+        partials.push([ op, n ]);
     });
 });
 
-for (var i = 0; i < partials.length; i++) {
-    var combs = combinations(partials, i + 1);
-    for (var j = 0; j < combs.length; j++) {
-        var f = solver.eval(combs[j]);
+for (var i = 1; i < 10000; i++) {
+    combinations(partials, i, function (comb) {
+        var f = solver.eval(comb);
         if (check(f)) {
-            console.dir(combs[j]);
-            console.log('[i,j]=' + JSON.stringify([i,j]));
-            console.log(new Date);
-            return;
+            console.dir(comb);
+            process.exit(0);
         };
-    }
+    });
 }
 
-function combinations (xs, n) {
-    if (n === 1) return xs.map(function (x) { return [x] });
-    var res = [];
+function combinations (xs, n, f) {
+    if (n === 1) return xs.forEach(function (x) { f([x]) });
     xs.forEach(function (x) {
-        combinations(xs, n-1).forEach(function (x_) {
-            res.push([x].concat(x_));
+        combinations(xs, n-1, function (x_) {
+            f([x].concat(x_));
         });
     });
-    return res;
 }
