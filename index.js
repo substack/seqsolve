c832f78639130adfcfad1cfcf6d5c815d5121758
James Halliday <mail@substack.net>
2013-06-14 15:31:14 -0700

    moved solver into lib/

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var permutationSolve = require('./lib/permutation.js');
-var gcd = require('gcd');
+var solve = require('./lib/solve.js');
 
 exports = module.exports = function (xs) {
     return evaluate(simplify(solve(xs)));
@@ -17,36 +16,4 @@ function simplify (terms) {
         if (t[0] === '-' && t[1] === 0) return false;
         return true;
     });
-}
-
-function solve (xs) {
-    var res = [];
-    
-    // linear
-    var slope = xs[1] - xs[0];
-    for (var i = 1; i < xs.length - 1; i++) {
-        if (xs[i+1] - xs[i] !== slope) break;
-    }
-    if (i === xs.length - 1) {
-        var b = xs[0] - slope;
-        return [ [ '*', slope ], [ '+', b ] ];
-    }
-    
-    // geometric
-    var mean = xs[1] / xs[0];
-    for (var i = 1; i < xs.length - 1; i++) {
-        if (xs[i+1] / xs[i] !== mean) break;
-    }
-    if (i === xs.length - 1) {
-        var x = xs[0] / mean;
-        return [ [ mean, '^' ], (x > 1 ? [ '*', x ] : [ '/', 1 / x ]) ];
-    }
-    
-    var factor = xs.reduce(gcd);
-    if (factor > 1) {
-        return [ ['*', factor] ].concat(permutationSolve(xs.map(function (x) {
-            return x / factor;
-        })));
-    }
-    else return permutationSolve(xs);
 }
