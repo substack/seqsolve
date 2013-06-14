1503b8477a139384b2718bc7e6b1304329475fd1
James Halliday <mail@substack.net>
2013-06-14 04:21:28 -0700

    fixed bugs in combs code, added n as a term

--- example/combinations.js
+++ example/combinations.js
@@ -1,9 +1,12 @@
 var cf = require('continued-fraction');
 var solver = require('../');
 var ops = [ '+', '-', '*', '/' ];
-var constants = [ 1, 2 ];
+var constants = [ 1, 2, 'n' ];
 
-var sequence = [ 0, 1, 6, 3, 1, 7, 2, 1, 3, 1, 1, 1, 2, 18, 1, 1, 1, 2 ];
+//var sequence = [ 0, 1, 6, 3, 1, 7, 2, 1, 3, 1, 1, 1, 2, 18, 1, 1, 1, 2 ];
+//var sequence = [ 0, 1, 2, 3, 4 ]; // SOLVED
+//var sequence = [ 2, 4, 8, 16, 32, 64 ]; // SOLVED
+var sequence = [ 1, 3, 5, 7, 9 ];
 var best = 0;
 
 function check (f) {
@@ -31,12 +34,12 @@ ops.forEach(function (op) {
 for (var i = 0; i < partials.length; i++) {
     var combs = combinations(partials, i + 1);
     for (var j = 0; j < combs.length; j++) {
-        var f = solver.eval(combs[i]);
+        var f = solver.eval(combs[j]);
         if (check(f)) {
-            console.dir(combs[i]);
+            console.dir(combs[j]);
             console.log('[i,j]=' + JSON.stringify([i,j]));
             console.log(new Date);
-            break;
+            return;
         };
     }
 }
