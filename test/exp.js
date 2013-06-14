5890926b1e295fc4c7478dd9da722503f134ffd1
James Halliday <mail@substack.net>
2013-06-14 05:35:31 -0700

    failing squared hundreds test

--- test/exp.js
+++ test/exp.js
@@ -10,3 +10,13 @@ test('squared', function (t) {
     for (var i = 0; i < xs.length; i++) res.push(f(i+1));
     t.deepEqual(res, xs);
 });
+
+test('squared hundreds', function (t) {
+    t.plan(1);
+    var xs = [ 100, 400, 900, 1600, 2500, 3600, 4900, 6400, 8100 ];
+    
+    var f = solver(xs);
+    var res = [];
+    for (var i = 0; i < xs.length; i++) res.push(f(i+1));
+    t.deepEqual(res, xs);
+});
