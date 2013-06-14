01871fb6fd9c5064b6e58150cdfd9743245535d1
James Halliday <mail@substack.net>
2013-06-14 15:22:55 -0700

    basic simplifier

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ var permutationSolve = require('./lib/permutation.js');
 var gcd = require('gcd');
 
 exports = module.exports = function (xs) {
-    return evaluate(solve(xs));
+    return evaluate(simplify(solve(xs)));
 };
-exports.solve = solve;
+exports.solve = function (xs) {
+    return simplify(solve(xs));
+};
 var evaluate = exports.eval = require('./lib/eval.js');
+
+function simplify (terms) {
+    return terms.filter(function (t) {
+        if (t[0] === '*' && t[1] === 1) return false;
+        if (t[0] === '/' && t[1] === 1) return false;
+        if (t[0] === '+' && t[1] === 0) return false;
+        if (t[0] === '-' && t[1] === 0) return false;
+        return true;
+    });
+}
 
 function solve (xs) {
     var res = [];
@@ -26,7 +38,8 @@ function solve (xs) {
         if (xs[i+1] / xs[i] !== mean) break;
     }
     if (i === xs.length - 1) {
-        return [ [ mean, '^' ] ];
+        var x = xs[0] / mean;
+        return [ [ mean, '^' ], (x > 1 ? [ '*', x ] : [ '/', 1 / x ]) ];
     }
     
     var factor = xs.reduce(gcd);
