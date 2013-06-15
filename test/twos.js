ef0b52905c7a32ec2c22dc2f32336fc599ae14af
James Halliday <mail@substack.net>
2013-06-14 21:40:40 -0700

    updated test

--- test/twos.js
+++ test/twos.js
@@ -5,12 +5,12 @@ test('linear slope', function (t) {
     t.plan(1);
     
     var xs = solver.solve([ 2, 4, 6, 8, 10, 12 ]);
-    t.deepEqual(xs, [ [ '+', 2 ] ]);
+    t.deepEqual(xs, [ [ '*', 2 ] ]);
 });
 
 test('geometric mean', function (t) {
     t.plan(1);
     
     var xs = solver.solve([ 2, 4, 8, 16, 32 ]);
-    t.deepEqual(xs, [ [ '*', 2 ] ]);
+    t.deepEqual(xs, [ [ 2, '^' ] ]);
 });
