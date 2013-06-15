28a8db60915725b93568c9248c4d49146dfe776e
James Halliday <mail@substack.net>
2013-06-14 21:17:48 -0700

    continue mode and expr mode

--- bin/solve.js
+++ bin/solve.js
@@ -1,10 +1,31 @@
 #!/usr/bin/env node
 
 var solver = require('../');
-var sequence = process.argv.slice(2).map(Number);
+var argv = require('optimist').boolean('c', 'continue').argv;
+
+var expr = argv.e || argv.expr;
+if (expr) {
+    var i = argv.i === undefined ? 1 : argv.i;
+    var f = Function('n', 'return ' + expr);
+    var end = i + (argv.n || (i + 10 - 1));
+    for (; i < end; i++) console.log(f(i));
+    return;
+}
+
+var sequence = argv._.map(function (n) { return parseInt(n, 10) });;
 
 var f = solver(sequence);
-console.log(pretty(f));
+
+if (argv.c || argv.continue) {
+    var i = sequence.length + 1;
+    var end = i + (argv.n || (i + 10 - 1));
+    for (; i < end; i++) {
+        console.log(f(i));
+    }
+}
+else {
+    console.log(pretty(f));
+}
 
 function pretty (f) {
     return (f+'').replace(/\n/g,'')
